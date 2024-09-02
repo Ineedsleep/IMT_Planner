a356f6e84a47bdb386040348237a2b6bbade8f95
Michael <needsleep@web.de>
2024-09-02 23:43:49 +0200

    #2 more Filter logic and service prep work, missing viewmodel and view

---
+++ IMT_Planner_DAL/Migrations/20240902214245_Add Mif bool.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMT_Planner_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMifbool : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasMultiplier",
+                table: "SuperManagers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasMultiplier",
+                table: "SuperManagers");
+        }
+    }
+}
