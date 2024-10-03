86bd9d4fa7f1ccebca3c506f835b9218e8595689
Michael <needsleep@web.de>
2024-10-03 19:59:55 +0200

    Fix filtered list

--- IMT_Planner_ViewModels/Services/SuperManagerSelectionService.cs
+++ IMT_Planner_ViewModels/Services/SuperManagerSelectionService.cs
@@ -294,6 +294,7 @@ public class SuperManagerSelectionService
         }
 
         NotifyFilterUpdate();
+        CollectionUpdate();
     }
 
     public ObservableCollection<SuperManagerCardViewModel> SortCollection(
